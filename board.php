dd5c5c5f9115af16a108fcee0dce86dd378100f3
Philipp Gruber <philipp.gruber@flupps.net>
2017-08-22 11:54:25 +1000

    fixing some width/height mixup

--- board.php
+++ board.php
@@ -7,16 +7,17 @@ class board {
 
     private $board = [];
 
-    public function __construct($width, $height) {
+    /* Initialize an empty board */
+    public function __construct($height, $width) {
 
         /* less than 3 will lead to overlapping neighboouring cells */
         if ($width < 3 or $height < 3) {
             throw new \RuntimeException('Width and height must be at least 3');
         }
 
-        for ($x = 0; $x < $width; $x++) {
+        for ($x = 0; $x < $height; $x++) {
             $this->board[$x] = [];
-            for ($y = 0; $y < $height; $y++) {
+            for ($y = 0; $y < $width; $y++) {
                 $this->board[$x][$y] = 0;
             }
         }
@@ -26,10 +27,11 @@ class board {
 
     }
 
+    /* set any cell on the board to 1 or 0 */
     public function set($x, $y, $value) {
         /* wrap board */
-        $x = $x % $this->width;
-        $y = $y % $this->height;
+        $x = $x % $this->height;
+        $y = $y % $this->width;
 
         /* enforce 0/1 integers to make counting easier */
         $value = $value ? 1 : 0;
@@ -37,14 +39,16 @@ class board {
         $this->board[$x][$y] = $value;
     }
 
+    /* fetch the value of any cell */
     public function get($x, $y) {
         /* wrap board */
-        $x = ($this->width+$x) % $this->width;
-        $y = ($this->height+$y) % $this->height;
+        $x = ($this->height+$x) % $this->height;
+        $y = ($this->width+$y) % $this->width;
 
         return $this->board[$x][$y];
     }
 
+    /* Return the number of neighbouring cells that are alive */
     private function neighbourCount($x, $y) {
         return $this->get($x-1, $y-1)
              + $this->get($x-1, $y  )
@@ -57,11 +61,12 @@ class board {
              ;
     }
 
+    /* Jump one step in time ahead */
     public function cycle() {
         $newBoard = [];
-        for ($x = 0; $x < $this->width; $x++) {
+        for ($x = 0; $x < $this->height; $x++) {
             $newBoard[] = [];
-            for ($y = 0; $y < $this->height; $y++) {
+            for ($y = 0; $y < $this->width; $y++) {
 
                 $newCell = $this->board[$x][$y];
 
@@ -86,12 +91,15 @@ class board {
         $this->board = $newBoard;
     }
 
+    /* Dump the board with 1337 ascii art */
     public function print() {
+        /* PHP Cli does not have an easy, cross-platform compatible clear screen :( */
+        echo str_repeat(PHP_EOL, 100);
         echo '╔'.str_repeat('═', $this->width).'╗'.PHP_EOL;
-        for ($x = 0; $x < $this->width; $x++) {
+        for ($x = 0; $x < $this->height; $x++) {
             echo '║';
-            for ($y = 0; $y < $this->height; $y++) {
-                echo $this->get($x, $y) ? 'X' : ' ';
+            for ($y = 0; $y < $this->width; $y++) {
+                echo $this->get($x, $y) ? '█' : ' ';
             }
             echo '║'.PHP_EOL;
         }
