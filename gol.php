13de54d455793b5bb9a46d0b624d31941dec3c35
Philipp Gruber <philipp.gruber@flupps.net>
2017-08-22 11:54:42 +1000

    some more sample code, more patterns

--- gol.php
+++ gol.php
@@ -2,15 +2,32 @@
 
 require_once 'board.php';
 
-$board = new Board(20, 20);
+$board = new Board(20, 40);
 
+/* oscillator */
+$board->set(12, 13, 1);
+$board->set(12, 14, 1);
+$board->set(12, 15, 1);
+
+/* still life */
+$board->set(34, 25, 1);
+$board->set(35, 24, 1);
+$board->set(35, 26, 1);
+$board->set(36, 25, 1);
+
+
+/* glider */
 $board->set(3, 3, 1);
+$board->set(4, 3, 1);
+$board->set(5, 3, 1);
 $board->set(3, 4, 1);
-$board->set(3, 5, 1);
+$board->set(4, 5, 1);
+
 
 $board->print();
-for ($i = 0; $i < 10; $i++) {
-    sleep(1);
+
+while (true) {
+    usleep(100000);
     $board->cycle();
     $board->print();
 }
